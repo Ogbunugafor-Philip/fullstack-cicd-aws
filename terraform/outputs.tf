eb6ca49bf59667d672b75ffa141027c61c3b273f
Philip Osita Ogbunugafor <ositaogbunugafor@gmail.com>
2025-08-04 18:30:02 +0100

    Fix: formatted outputs.tf to pass terraform fmt check

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,7 +5,7 @@ output "s3_bucket_name" {
 
 output "s3_website_url" {
   description = "Public URL to access the frontend website"
-  value       =  aws_s3_bucket_website_configuration.frontend_bucket_website.website_endpoint
+  value       = aws_s3_bucket_website_configuration.frontend_bucket_website.website_endpoint
 }
 
 output "cloudfront_url" {
